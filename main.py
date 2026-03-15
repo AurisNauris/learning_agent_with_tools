5d665926669bb6492a0c53849b0c5d4201540429
Aurimas <aurimasnarkevicius@gmail.com>
2026-03-15 15:22:48 +0100

    The tool result processing into LLM

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import anthropic
 from dotenv import load_dotenv
-
+import sys
 load_dotenv()
 
 tools = [
@@ -31,3 +31,31 @@ response = client.messages.create(model="claude-haiku-4-5-20251001",
                                       "content":"What is 347*23?"
                                      }])
 
+if response.stop_reason == "tool_use":
+    tool_block = response.content[0]
+    tool_name = tool_block.name
+    tool_input = tool_block.input
+    
+    if tool_name == "calculator":
+        result = str(eval(tool_input["expression"]))
+else:
+    print("failed tool use")
+    sys.exit()
+
+followup = client.messages.create(model="claude-haiku-4-5-20251001",
+                                  max_tokens=1024,
+                                  tools=tools,
+                                  messages=[
+                                      {"role":"user", "content":"What is 347*23?"},
+                                      {"role":"assistant", "content":response.content},
+                                      {"role":"user",
+                                       "content": [
+                                           {"type":"tool_result",
+                                            "tool_use_id": tool_block.id,
+                                            "content":result
+                                            }
+                                       ]}
+                                  ])
+
+print(followup.content[0].text)
+print(followup)
