5e5a55517242e101cd1ec8bde3fe86ba77efdf98
Aurimas <aurimasnarkevicius@gmail.com>
2026-03-15 16:19:53 +0100

    Interactive conversation with LLM with tool use

--- main.py
+++ main.py
@@ -74,55 +74,61 @@ client = anthropic.Anthropic()
 
 i = 0
 prompt = []
-prompt.append({"role":"user",
-               "content":"What is the weather in Dresden? and what is the temperature times 3?" })
+# prompt.append({"role":"user",
+#                "content":"What is the weather in Dresden? and what is the temperature times 3?" })
 
 while True:
-    i += 1
-    if i > 10: # to avoid accidental infinite loop for now
+
+    user_input = input("You: ")
+    if user_input.lower() in ["quit","exit"]:
         break
+    prompt.append({"role":"user","content":user_input})
 
-    response = client.messages.create(model="claude-haiku-4-5-20251001",
-                                      max_tokens=1024,
-                                      tools=tools,
-                                      messages=prompt)
-    
-    if response.stop_reason == "tool_use":
-        print("Using a tool")
-        print("response.contet")
-        print(response.content)
-        tool_block = None
-        for block in response.content:
-            if block.type == "tool_use":
-                tool_block = block
-                break
+    while True:
+        i += 1
+        if i > 20: # to avoid accidental infinite loop for now
+            break
 
-       # tool_block = response.content[0]
-        tool_name = tool_block.name
-        tool_input = tool_block.input
+        response = client.messages.create(model="claude-haiku-4-5-20251001",
+                                        max_tokens=1024,
+                                        tools=tools,
+                                        messages=prompt)
         
-        if tool_name == "calculator":
-            result = str(eval(tool_input["expression"]))
-            prompt.append({"role":"assistant", "content":response.content})
-            prompt.append({"role":"user",
-                           "content":[{
-                               "type":"tool_result",
-                               "tool_use_id":tool_block.id,
-                               "content":result
-                           }]})
-        if tool_name == "get_weather":
-            result = "22"
-            prompt.append({"role":"assistant","content":response.content})
-            prompt.append({"role":"user",
-                           "content":[{
-                               "type":"tool_result",
-                               "tool_use_id": tool_block.id,
-                               "content": result
-                           }]})
-    
-    if response.stop_reason == "end_turn":
-        print("Conversation is over")
-        print(response.content[0].text)
-        break
+        for block in response.content:
+            if block.type == "text":
+                print("Claude: ", block.text)
 
-    time.sleep(0.5)
+        if response.stop_reason == "end_turn":
+            prompt.append({"role":"assistant",
+                           "content":response.content})
+            break
+
+        if response.stop_reason == "tool_use":
+            tool_block = None
+            for block in response.content:
+                if block.type == "tool_use":
+                    tool_block = block
+                    break
+            tool_name = tool_block.name
+            tool_input = tool_block.input
+            
+            if tool_name == "calculator":
+                result = str(eval(tool_input["expression"]))
+                prompt.append({"role":"assistant", "content":response.content})
+                prompt.append({"role":"user",
+                            "content":[{
+                                "type":"tool_result",
+                                "tool_use_id":tool_block.id,
+                                "content":result
+                            }]})
+            if tool_name == "get_weather":
+                result = "22"
+                prompt.append({"role":"assistant","content":response.content})
+                prompt.append({"role":"user",
+                            "content":[{
+                                "type":"tool_result",
+                                "tool_use_id": tool_block.id,
+                                "content": result
+                            }]})
+
+        time.sleep(0.5)
